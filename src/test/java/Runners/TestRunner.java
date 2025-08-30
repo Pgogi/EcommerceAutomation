ab1d65ca0e5f4050c3bd83d3c3d01586fa69a603
Prabhakar Gogi <prabhakargogi543@gmail.com>
2025-08-30 13:18:11 +0530

    updated 2

--- src/test/java/Runners/TestRunner.java
+++ src/test/java/Runners/TestRunner.java
@@ -1,22 +1,21 @@
 package Runners;
 
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+//import io.cucumber.junit.Cucumber;
+//import io.cucumber.junit.CucumberOptions;
 
-@RunWith(Cucumber.class)
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+//@RunWith(Cucumber.class)
 @CucumberOptions(
+	    features = "src/test/resources/features",
+	    glue = "StepDefinition",
+	    dryRun = false,
+	    monochrome = true
+	)
 
-		features = "src/test/java/features",
-
-		glue = "StepDefinition",
-
-		dryRun = false,
-
-		monochrome = true
-
-		)
-public class TestRunner {
+public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
